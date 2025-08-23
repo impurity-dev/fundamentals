c4898dca64baf9388347357ec333ab2f49a65025
impurity-dev <techkoko77@gmail.com>
2025-08-23 18:16:50 -0400

    Fix hooks

--- setup.ts
+++ setup.ts
@@ -1,10 +1,9 @@
-const p = Deno.run({
-    cmd: ['git', 'config', 'core.hooksPath', '.githooks'],
+const command = new Deno.Command('git', {
+    args: ['config', 'core.hooksPath', '.githooks'],
 });
-const status = await p.status();
-if (status.success) {
+const { code } = await command.output();
+if (code === 0) {
     console.log('✅ Git hooks path set to .githooks');
 } else {
     console.error('❌ Failed to set git hooks path');
 }
-p.close();
